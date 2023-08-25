c854d420a18c64b73e0c5b7f28c7c4288ae95db0
moodybraima <132967620+moodybraima@users.noreply.github.com>
2023-08-25 00:04:31 -0500

    Add files via upload

---
+++ todo-list/index.js
@@ -0,0 +1,50 @@
+function App(){
+  const [todos, setTodos] = React.useState([
+    {
+      text: 'learn react',
+      isCompleted: false,
+    },
+    {
+      text: 'take my wife out to dinner',
+      isCompleted: false,
+    },
+    {
+      text: 'go to the park',
+      isCompleted: false,
+    },
+    {
+      text: 'read the newspaper',
+      isCompleted: false,
+    },
+    {
+      text: 'go swimming tomorrow',
+      isCompleted: false,
+    }        
+  ]);
+
+  const addTodo = text => {
+    const newTodos = [...todos, {text, isCompleted:false}];
+    setTodos(newTodos);
+  }
+  const removeTodo = index => {
+    let temp = [...todos];    
+    temp.splice(index, 1);
+    setTodos(temp);
+  }
+
+  return(
+    <div className="app">
+      <div className="todo-list" >
+        {todos.map((todo, i) => (
+          <Todo key={i} index={i} todo={todo} remove={removeTodo}/>
+        ))}
+        <TodoForm addTodo={addTodo} />
+      </div>
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('root')
+);
